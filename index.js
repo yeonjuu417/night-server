ee0de9b66aae4b14fbc77ae6ae37ae96ff632dbd
Ubuntu <ubuntu@ip-172-31-45-244.ap-northeast-2.compute.internal>
2020-12-29 06:00:25 +0000

    Add redirect http -> https

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const socialsRouter = require('./routes/social');
 
 const port = 3001;//포트번호수정 !!!!!!!
 
+app.use(function(req, res, next){
+	if (!req.secure){
+		res.redirect("https://onemeal.site"+req.url);
+	}else{
+		next();
+	}
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended : true }));
 app.use(logger('dev'));
