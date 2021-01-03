042c02199ee4f06c756bdedd172c0a73ecbc6ef5
jeongyeonju <yeonjuu417@icloud.com>
2021-01-03 18:16:28 +0900

    userinfoup_fix

--- controller/users/userinfoup.js
+++ controller/users/userinfoup.js
@@ -1,4 +1,5 @@
 const { users } = require('../../models');
+const crypto = require('crypto');
 const user = require('../../models/user');
 
 module.exports = {
@@ -17,11 +18,31 @@ module.exports = {
             })
         }
         if(req.session.userId && password){
+
+            const createSalt = () =>
+            new Promise((resolve, reject) => {
+                crypto.randomBytes(64, (err, buf) => {
+                    if (err) reject(err);
+                    resolve(buf.toString('base64'));
+                });
+            });
+
+            const createHashedPassword = (plainpassword) =>
+            new Promise(async (resolve, reject) => {
+                const salt = await createSalt();
+                crypto.pbkdf2(plainpassword, salt, 1000, 64, 'sha512', (err, key) => {
+                    if (err) reject(err);
+                    resolve({ hashPwd: key.toString('base64'), salt });
+                });
+            });
+
+            const { hashPwd, salt } = await createHashedPassword(password);
+
             await users.findOne({
                 where : {id : req.session.userId}
             })
             .then(data =>{
-                users.update({password : password},{where : {id : req.session.userId}})
+                users.update({password : hashPwd, salt: salt},{where : {id : req.session.userId}})
                 res.status(302).send({data: user.username})
             }).catch(err => {
                 res.status(404).send({message: "user not found"});
